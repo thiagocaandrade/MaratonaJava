a45d6641391257c0b8a2053dc3333871358a3c40
Thiago Andrade <thiago.carlos.andrade@gmail.com>
2021-04-01 17:29:16 -0300

    codigo ate a aula 67

---
+++ src/br/com/thiago/javacore/Oexception/error/test/ErrorTest.java
@@ -0,0 +1,11 @@
+package br.com.thiago.javacore.Oexception.error.test;
+
+public class ErrorTest {
+    public static void main(String[] args) {
+        stackOverFlowError();
+    }
+
+    public static void stackOverFlowError(){
+        stackOverFlowError();
+    }
+}
---
+++ src/br/com/thiago/javacore/Oexception/runtimeexception/test/RuntimeExceptionTest.java
@@ -0,0 +1,7 @@
+package br.com.thiago.javacore.Oexception.runtimeexception.test;
+
+public class RuntimeExceptionTest {
+    public static void main(String[] args) {
+
+    }
+}
